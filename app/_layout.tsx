ccb77e7a6dc8f75a90b96353f55c0d08ca8d7e24
Ahmad Saefudin <adsnfifthkage3@gmail.com>
2024-06-22 00:22:24 +0700

    chore - add notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,22 @@ import { useFonts } from 'expo-font';
 import { Stack,router } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import 'react-native-reanimated';
+import * as Notifications from 'expo-notifications';
+import usePushNotification from '@/hooks/usePushNotification';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -20,16 +30,28 @@ export default function RootLayout() {
     PoppinsBold: require('../assets/fonts/Poppins-Bold.ttf'),
   });
 
+  const {registerForPushNotificationsAsync} = usePushNotification()
   const {account} = useSession();
 
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
-      if (!account || account == null) {
-        router.replace('/login')
-      }
+      onAppLoaded()
     }
   }, [account, loaded]);
+
+  const onAppLoaded = useCallback(()=>{
+    if (!account || account == null) {
+      router.replace('/login')
+      return;
+    }
+
+    registerForPushNotificationsAsync().then(token=>{
+      console.log(token);
+    }).catch(e=>{
+      console.log(e);
+    })
+  },[account])
 
   if (!loaded) {
     return null;
